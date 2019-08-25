b8c17a08b255c6b7422efff195996a18ab03b4d6
dli <ihgazni2010@hotmail.com>
2019-08-25 06:47:50 +0000

    npm

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: "webidl2.js",
+    filename: "webidl2-mozilla-compatible.js",
     path: path.resolve(__dirname, "dist"),
     library: "WebIDL2",
     libraryTarget: "umd",
